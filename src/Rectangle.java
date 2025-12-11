70f3571f443e8af36e1e2acbb65d4d94c17d4d27
meinetwegen <hiasabeatytv@gmail.com>
2025-12-11 10:17:17 +0500

    Rectangle class added

---
+++ src/Rectangle.java
@@ -0,0 +1,57 @@
+class Rectangle {
+    private double width;
+    private double height;
+    private int id;
+    private static int idGen = 1; //Спросить на паре: тут 0 или 1(при создании первый будет с айди 1?)
+
+    public Rectangle() {
+        this.width = 1.0;
+        this.height = 1.0;
+        this.id = idGen++;
+    }
+
+    public Rectangle(double width, double height) {
+        this();
+        setWidth(width);
+        setHeight(height);
+    }
+
+    public double getWidth() {
+        return width;
+    }
+
+    public double getHeight() {
+        return height;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setWidth(double width) {
+        if (width <= 0) {
+            throw new IllegalArgumentException("Width must be greater than 0");
+        }
+        this.width = width;
+    }
+
+    public void setHeight(double height) {
+        if (height <= 0) {
+            throw new IllegalArgumentException("Height must be greater than 0");
+        }
+        this.height = height;
+    }
+
+    public double area() {
+        return this.width * this.height;
+    }
+
+    public double perimeter() {
+        return 2 * (this.width + this.height);
+    }
+
+    @Override
+    public String toString() {
+        return "Rectangle: id=" + id + ", width=" + width + ", height=" + height;
+    }
+}
